b75babd65bc43b04096934bf201375a58b63bb2d
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2016-04-30 21:10:10 +0200

    added the subject

---
+++ NUnit/ConductOfCode/ConductOfCode/HelloWorld.cs
@@ -0,0 +1,29 @@
+﻿namespace ConductOfCode
+{
+    public class HelloWorld
+    {
+        private readonly IFoo _foo;
+        private readonly IBar _bar;
+
+        public HelloWorld(IFoo foo, IBar bar)
+        {
+            _foo = foo;
+            _bar = bar;
+        }
+
+        public string GetMessage()
+        {
+            return _foo.GetFoo() + _bar.GetBar();
+        }
+    }
+
+    public interface IFoo
+    {
+        string GetFoo();
+    }
+
+    public interface IBar
+    {
+        string GetBar();
+    }
+}
