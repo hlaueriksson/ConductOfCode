3c468931d7eb71e0fff2840200a25f67f5a0d273
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2016-07-30 21:13:10 +0200

    :sparkles: SpecFlow

---
+++ BDD/ConductOfCode/ConductOfCode/SpecFlow/README.md
@@ -0,0 +1,5 @@
+# SpecFlow
+
+* Dependencies: NUnit | xUnit | MsTest
+* NuGet: https://www.nuget.org/packages/SpecFlow/
+* Source: https://github.com/techtalk/SpecFlow
---
+++ BDD/ConductOfCode/ConductOfCode/SpecFlow/StackSteps.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace ConductOfCode.SpecFlow
+{
+    [Binding]
+    public class StackSteps
+    {
+        private Stack<int> stack;
+
+        private int result;
+
+        // Empty
+
+        [Given(@"an empty stack")]
+        public void GivenAnEmptyStack()
+        {
+            stack = new Stack<int>();
+        }
+
+        [Then(@"it has no elements")]
+        public void ThenItHasNoElements()
+        {
+            Assert.Empty(stack);
+        }
+
+        [Then(@"it throws an exception when calling pop")]
+        public void ThenItThrowsAnExceptionWhenCallingPop()
+        {
+            Assert.Throws<InvalidOperationException>(() => stack.Pop());
+        }
+
+        [Then(@"it throws an exception when calling peek")]
+        public void ThenItThrowsAnExceptionWhenCallingPeek()
+        {
+            Assert.Throws<InvalidOperationException>(() => stack.Peek());
+        }
+
+        // Not empty
+
+        [Given(@"a non empty stack")]
+        public void GivenANonEmptyStack()
+        {
+            stack = new Stack<int>(new[] { 1, 2, 3 });
+        }
+
+        [When(@"calling peek")]
+        public void WhenCallingPeek()
+        {
+            result = stack.Peek();
+        }
+
+        [Then(@"it returns the top element")]
+        public void ThenItReturnsTheTopElement()
+        {
+            Assert.Equal(3, result);
+        }
+
+        [Then(@"it does not remove the top element")]
+        public void ThenItDoesNotRemoveTheTopElement()
+        {
+            Assert.Contains(3, stack);
+        }
+
+        [When(@"calling pop")]
+        public void WhenCallingPop()
+        {
+            result = stack.Pop();
+        }
+
+        [Then(@"it removes the top element")]
+        public void ThenItRemovesTheTopElement()
+        {
+            Assert.DoesNotContain(3, stack);
+        }
+    }
+}
