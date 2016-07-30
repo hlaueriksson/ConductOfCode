33631d54be3986b6263d7a22fd2888513a920ea6
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2016-07-30 21:00:20 +0200

    :sparkles: Concordion

---
+++ BDD/ConductOfCode/ConductOfCode/Concordion/README.md
@@ -0,0 +1,5 @@
+# Concordion.NET
+
+* Dependencies: NUnit
+* NuGet: https://www.nuget.org/packages/Concordion.NET/
+* Source: https://github.com/concordion/concordion.net
---
+++ BDD/ConductOfCode/ConductOfCode/Concordion/StackTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Concordion.NET.Integration;
+using Concordion.Runners.NUnit;
+using NUnit.Framework;
+
+namespace ConductOfCode.Concordion
+{
+    [TestFixture]
+    [ConcordionTest]
+    public class StackTest : ExecutableSpecification
+    {
+        private Stack<int> stack;
+
+        public int TopElement => 3;
+
+        // Empty
+
+        public void GivenAnEmptyStack()
+        {
+            stack = new Stack<int>();
+        }
+
+        public bool IsEmpty()
+        {
+            return stack.Count == 0;
+        }
+
+        public bool ShouldThrowWhenPop()
+        {
+            return ShouldThrowWhen(() => stack.Pop());
+        }
+
+        public bool ShouldThrowWhenPeek()
+        {
+            return ShouldThrowWhen(() => stack.Peek());
+        }
+
+        private bool ShouldThrowWhen(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // Not empty
+
+        public void GivenANonEmptyStack()
+        {
+            stack = new Stack<int>(new[] { 1, 2, TopElement });
+        }
+
+        public int Pop()
+        {
+            return stack.Pop();
+        }
+
+        public int Peek()
+        {
+            return stack.Peek();
+        }
+
+        public bool Contains(int value)
+        {
+            return stack.Contains(value);
+        }
+
+        public bool DoesNotContain(int value)
+        {
+            return !stack.Contains(value);
+        }
+    }
+}
