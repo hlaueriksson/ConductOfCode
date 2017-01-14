accb90bd8d788a071bc8f7289d5f3a32273f41a7
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2017-01-14 21:54:00 +0100

    implemented Api steps

---
+++ SpecFlow/ConductOfCode.Specs/ConductOfCode.Specs.Api/StackFacade.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ConductOfCode.Specs.Clients;
+
+namespace ConductOfCode.Specs
+{
+    public class StackFacade
+    {
+        private readonly StackClient _client;
+
+        public StackFacade()
+        {
+            _client = new StackClient();
+        }
+
+        public void Clear()
+        {
+            _client.ClearAsync().Wait();
+        }
+
+        public string Peek()
+        {
+            return _client.PeekAsync().Result.Value;
+        }
+
+        public string Pop()
+        {
+            return _client.PopAsync().Result.Value;
+        }
+
+        public void Push(string item)
+        {
+            _client.PushAsync(new Item { Value = item }).Wait();
+        }
+
+        public IEnumerable<string> ToArray()
+        {
+            return _client.ToArrayAsync().Result.Select(x => x.Value);
+        }
+    }
+}
--- SpecFlow/ConductOfCode.Specs/ConductOfCode.Specs.Api/StackSteps.cs
+++ SpecFlow/ConductOfCode.Specs/ConductOfCode.Specs.Api/StackSteps.cs
@@ -1,58 +1,87 @@
-﻿using TechTalk.SpecFlow;
+﻿using System;
+using ConductOfCode.Specs.Clients;
+using Should;
+using TechTalk.SpecFlow;
+using Xunit;
 
 namespace ConductOfCode.Specs
 {
     [Binding]
     public class StackSteps : BaseSteps
     {
+        private StackFacade facade;
+
+        private string result;
+
+        // Empty
+
         [Given(@"an empty stack")]
         public void GivenAnEmptyStack()
         {
+            facade = new StackFacade();
+            facade.Clear();
         }
-        
-        [Given(@"a non empty stack")]
-        public void GivenANonEmptyStack()
-        {
-        }
-        
-        [When(@"calling peek")]
-        public void WhenCallingPeek()
-        {
-        }
-        
-        [When(@"calling pop")]
-        public void WhenCallingPop()
-        {
-        }
-        
+
         [Then(@"it has no elements")]
         public void ThenItHasNoElements()
         {
+            facade.ToArray().ShouldBeEmpty();
         }
-        
+
         [Then(@"it throws an exception when calling pop")]
         public void ThenItThrowsAnExceptionWhenCallingPop()
         {
+            var exception = Assert.Throws<AggregateException>(() => facade.Pop());
+            exception.InnerException.ShouldBeType<SwaggerException<Error>>();
         }
-        
+
         [Then(@"it throws an exception when calling peek")]
         public void ThenItThrowsAnExceptionWhenCallingPeek()
         {
+            var exception = Assert.Throws<AggregateException>(() => facade.Peek());
+            exception.InnerException.ShouldBeType<SwaggerException<Error>>();
         }
-        
+
+        // Not empty
+
+        [Given(@"a non empty stack")]
+        public void GivenANonEmptyStack()
+        {
+            facade = new StackFacade();
+            facade.Clear();
+            facade.Push("1");
+            facade.Push("2");
+            facade.Push("3");
+        }
+
+        [When(@"calling peek")]
+        public void WhenCallingPeek()
+        {
+            result = facade.Peek();
+        }
+
         [Then(@"it returns the top element")]
         public void ThenItReturnsTheTopElement()
         {
+            result.ShouldEqual("3");
         }
-        
+
         [Then(@"it does not remove the top element")]
         public void ThenItDoesNotRemoveTheTopElement()
         {
+            facade.ToArray().ShouldContain("3");
         }
-        
+
+        [When(@"calling pop")]
+        public void WhenCallingPop()
+        {
+            result = facade.Pop();
+        }
+
         [Then(@"it removes the top element")]
         public void ThenItRemovesTheTopElement()
         {
+            facade.ToArray().ShouldNotContain("3");
         }
     }
 }
