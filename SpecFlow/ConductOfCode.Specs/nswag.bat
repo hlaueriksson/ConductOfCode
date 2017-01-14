733981cd2b2322b6993effd5e2a616862f31da19
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2017-01-14 20:42:35 +0100

    added NSwag.MSBuild

---
+++ SpecFlow/ConductOfCode.Specs/nswag.bat
@@ -0,0 +1,2 @@
+:: NSwag
+.\packages\NSwag.MSBuild.8.5.0\build\NSwag.exe swagger2csclient /input:http://localhost:5000/swagger/v1/swagger.json /classname:StackClient /namespace:ConductOfCode.Specs.Clients /output:./ConductOfCode.Specs.Api/Clients/StackClient.cs
