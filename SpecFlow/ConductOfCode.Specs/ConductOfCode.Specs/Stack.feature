7196f0377812719be9e6e15323593bc04fc6f163
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2017-01-14 16:29:03 +0100

    added feature

---
+++ SpecFlow/ConductOfCode.Specs/ConductOfCode.Specs/Stack.feature
@@ -0,0 +1,19 @@
+﻿Feature: Stack
+	In order to support last-in-first-out (LIFO) operations
+	As an developer
+	I want to use a stack
+
+Scenario: Empty stack
+	Given an empty stack
+	Then it has no elements
+	And it throws an exception when calling pop
+	And it throws an exception when calling peek
+
+Scenario: Non empty stack
+	Given a non empty stack
+	When calling peek
+	Then it returns the top element
+	But it does not remove the top element
+	When calling pop
+	Then it returns the top element
+	And it removes the top element
---
+++ SpecFlow/ConductOfCode.Specs/ConductOfCode.Specs/StackSteps.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Should;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace ConductOfCode.Specs
+{
+    [Binding]
+    public class StackSteps
+    {
+        private Stack<int> stack;
+
+        private int result;
+
+        // Empty
+
+        [Given(@"an empty stack")]
+        public void GivenAnEmptyStack()
+        {
+            stack = new Stack<int>();
+        }
+
+        [Then(@"it has no elements")]
+        public void ThenItHasNoElements()
+        {
+            stack.ShouldBeEmpty();
+        }
+
+        [Then(@"it throws an exception when calling pop")]
+        public void ThenItThrowsAnExceptionWhenCallingPop()
+        {
+            Assert.Throws<InvalidOperationException>(() => stack.Pop());
+        }
+
+        [Then(@"it throws an exception when calling peek")]
+        public void ThenItThrowsAnExceptionWhenCallingPeek()
+        {
+            Assert.Throws<InvalidOperationException>(() => stack.Peek());
+        }
+
+        // Not empty
+
+        [Given(@"a non empty stack")]
+        public void GivenANonEmptyStack()
+        {
+            stack = new Stack<int>(new[] { 1, 2, 3 });
+        }
+
+        [When(@"calling peek")]
+        public void WhenCallingPeek()
+        {
+            result = stack.Peek();
+        }
+
+        [Then(@"it returns the top element")]
+        public void ThenItReturnsTheTopElement()
+        {
+            result.ShouldEqual(3);
+        }
+
+        [Then(@"it does not remove the top element")]
+        public void ThenItDoesNotRemoveTheTopElement()
+        {
+            stack.ShouldContain(3);
+        }
+
+        [When(@"calling pop")]
+        public void WhenCallingPop()
+        {
+            result = stack.Pop();
+        }
+
+        [Then(@"it removes the top element")]
+        public void ThenItRemovesTheTopElement()
+        {
+            stack.ShouldNotContain(3);
+        }
+    }
+}
