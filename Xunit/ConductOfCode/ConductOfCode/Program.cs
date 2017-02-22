d5a69c57bcd6f43e2ed2496956c2243ad1134114
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2017-02-22 21:54:00 +0100

    added Xunit solution

---
+++ Xunit/ConductOfCode/ConductOfCode/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConductOfCode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
