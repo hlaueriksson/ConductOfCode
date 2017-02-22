81b421fac9aaede19e6c0b8b3234b6da1b2eb103
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2017-02-22 22:04:08 +0100

    added Core project

---
+++ Xunit/ConductOfCode/ConductOfCode.Core/HelloWorld.cs
@@ -0,0 +1,44 @@
+﻿namespace ConductOfCode.Core
+{
+    public interface IHelloWorld
+    {
+        string GetMessage();
+    }
+
+    internal class HelloWorld : IHelloWorld
+    {
+        private readonly IFoo _foo;
+        private readonly IBar _bar;
+
+        public HelloWorld(IFoo foo, IBar bar)
+        {
+            _foo = foo;
+            _bar = bar;
+        }
+
+        public string GetMessage()
+        {
+            return _foo.GetFoo() + _bar.GetBar();
+        }
+    }
+
+    public interface IFoo
+    {
+        string GetFoo();
+    }
+
+    internal class Foo : IFoo
+    {
+        public string GetFoo() => "Hello";
+    }
+
+    public interface IBar
+    {
+        string GetBar();
+    }
+
+    internal class Bar : IBar
+    {
+        public string GetBar() => ", World!";
+    }
+}
---
+++ Xunit/ConductOfCode/ConductOfCode.Core/HelloWorldRegistry.cs
@@ -0,0 +1,14 @@
+﻿using StructureMap;
+
+namespace ConductOfCode.Core
+{
+    public class HelloWorldRegistry : Registry
+    {
+        public HelloWorldRegistry()
+        {
+            For<IHelloWorld>().Use<HelloWorld>();
+            For<IFoo>().Use<Foo>();
+            For<IBar>().Use<Bar>();
+        }
+    }
+}
