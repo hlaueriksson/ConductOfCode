f695a29483be9d1c1c2fc067a485843d1ee94284
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2017-03-27 21:41:24 +0200

    added Swashbuckle

--- JWT/ConductOfCode/ConductOfCode/Startup.cs
+++ JWT/ConductOfCode/ConductOfCode/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ConductOfCode
 {
@@ -25,6 +26,11 @@ namespace ConductOfCode
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "ConductOfCode", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +38,13 @@ namespace ConductOfCode
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ConductOfCode");
+            });
 
             app.UseMvc();
         }
