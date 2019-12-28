9fb9520e8cdf916939261b1e5c14df0ddd0e43dc
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2019-12-28 23:32:14 +0100

    LoFuUnit.AutoNSubstitute

---
+++ LoFuUnit/ConductOfCode/ConductOfCode/HelloWorldTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using LoFuUnit.AutoNSubstitute;
+using LoFuUnit.NUnit;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ConductOfCode
+{
+    public class HelloWorldTests : LoFuTest<HelloWorld>
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Use<IFoo>().GetFoo().Returns("Hello");
+            Use(Substitute.For<IBar>()).GetBar().Returns(", World!");
+        }
+
+        [LoFu, Test]
+        public void When_GetMessage()
+        {
+            Result = Subject.GetMessage();
+
+            void should_invoke_IFoo_GetMessage() =>
+                The<IFoo>().Received().GetFoo();
+            void should_invoke_IBar_GetMessage() =>
+                The<IBar>().Received(1).GetBar();
+            void should_return_a_concatenated_string_with_messages_from_IFoo_and_IBar() =>
+                Result.Should().Be("Hello, World!");
+        }
+
+        string Result { get; set; }
+    }
+}
