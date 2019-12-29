35b406804a4d8905eaea6e469be705bd4cc80c55
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2019-12-29 18:30:40 +0100

    linebreaks

--- LoFuUnit/ConductOfCode/ConductOfCode/StackTests.cs
+++ LoFuUnit/ConductOfCode/ConductOfCode/StackTests.cs
@@ -13,9 +13,12 @@ namespace ConductOfCode
         {
             Subject = new Stack<int>();
 
-            void should_have_no_elements() => Subject.Should().BeEmpty();
-            void should_throw_an_exception_when_calling__Pop__() => Subject.Invoking(y => y.Pop()).Should().Throw<InvalidOperationException>();
-            void should_throw_an_exception_when_calling__Peek__() => Subject.Invoking(y => y.Peek()).Should().Throw<InvalidOperationException>();
+            void should_have_no_elements() =>
+                Subject.Should().BeEmpty();
+            void should_throw_an_exception_when_calling__Pop__() =>
+                Subject.Invoking(y => y.Pop()).Should().Throw<InvalidOperationException>();
+            void should_throw_an_exception_when_calling__Peek__() =>
+                Subject.Invoking(y => y.Peek()).Should().Throw<InvalidOperationException>();
         }
 
         [LoFu, Test]
@@ -23,13 +26,15 @@ namespace ConductOfCode
         {
             Subject = new Stack<int>(new[] { 1, 2, 3 });
 
-            void should_return_the_top_element_when_calling__Peek__() => Subject.Peek().Should().Be(3);
+            void should_return_the_top_element_when_calling__Peek__() =>
+                Subject.Peek().Should().Be(3);
             void should_not_remove_the_top_element_when_calling__Peek__()
             {
                 var result = Subject.Peek();
                 Subject.Should().Contain(result);
             }
-            void should_return_the_top_element_when_calling__Pop__() => Subject.Pop().Should().Be(3);
+            void should_return_the_top_element_when_calling__Pop__() =>
+                Subject.Pop().Should().Be(3);
             void should_remove_the_top_element_when_calling__Pop__()
             {
                 var result = Subject.Pop();
